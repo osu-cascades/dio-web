636a57cff3b67163bcf85adc3fb0c31bfa8f9ffe
JWTappert <tappertj@oregonstate.edu>
2018-01-19 14:59:43 -0800

    Adds routing

---
+++ src/app/app-router.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {AppComponent} from "./app.component";
+import {GraphComponent} from "./graph/graph.component";
+
+const routes: Routes = [
+  {
+    path: '', component: AppComponent
+  },
+  {
+    path: 'graph', component: GraphComponent
+  },
+  {
+    path: '**', redirectTo: '/'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GrpahComponent } from './grpah/grpah.component';
+import { GraphComponent } from './graph/graph.component';
+import {AppRoutingModule} from "./app-router.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    GrpahComponent
+    GraphComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
