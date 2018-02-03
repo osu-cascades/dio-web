a9414558d736438d9143f380f39fb78336768afc
Justin Tappert <justin@theflybook.com>
2018-02-03 11:57:39 -0800

    missed: changes data type of request

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -20,7 +20,7 @@ export class ApiService implements OnInit {
 
   getReadingsInRange(request: DateRangeRequest) {
     const options = request ? { params: new HttpParams().set('start', request.startDate).set('end', request.endDate) } : {};
-    return this.http.get<DateRangeRequest>(`https://vv-dio-service-staging.herokuapp.com/api/v1/do/readings/query`, options);
+    return this.http.get<Reading[]>(`https://vv-dio-service-staging.herokuapp.com/api/v1/do/readings/query`, options);
   }
 
   ngOnInit(): void {
