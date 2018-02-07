995110680b636cdd7cec93976b1b8d03edbc7530
Justin Tappert <justin@theflybook.com>
2018-02-06 22:15:00 -0800

    One more time

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.use(function(req, res) {
-  res.sendFile(__dirname + '/src/index.html');
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
