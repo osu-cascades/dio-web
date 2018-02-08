0866a4f439e9699d2a76303b70e1fadd863fa761
Justin Tappert <justin@theflybook.com>
2018-02-07 22:36:35 -0800

    Adding datetime utils service

--- src/app/services/datetime.service.ts
+++ src/app/services/datetime.service.ts
@@ -1,6 +1,17 @@
 import {Injectable} from '@angular/core';
+import * as moment from 'moment';
 
 @Injectable()
 export class DatetimeService {
+  MySqlFormat = 'YYYY-MM-DD HH:MM:ss';
 
+  constructor() {}
+
+  getEarliestTimeOnDay(datetime: string) {
+    return moment(datetime).startOf('day').format(this.MySqlFormat);
+  }
+
+  getLatestTimeOnday(datetime: string) {
+    return moment(datetime).endOf('day').format(this.MySqlFormat);
+  }
 }
